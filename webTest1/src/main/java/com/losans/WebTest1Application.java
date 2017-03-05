42599b72cfb0ff8cf1dfeb746a13683073e51e5a
Zareen <Zareen@Zareen-PC>
2017-03-05 12:29:32 +0530

    adding space

--- webTest1/src/main/java/com/losans/WebTest1Application.java
+++ webTest1/src/main/java/com/losans/WebTest1Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebTest1Application {
 
-	
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebTest1Application.class, args);
 	}
